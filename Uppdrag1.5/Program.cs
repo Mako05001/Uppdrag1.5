c3ceca8e8b06149c9b23a710512dfc23dfb7a413
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-02 15:02:02 +0200

    Add project files.

---
+++ Uppdrag1.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Uppdrag1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\.");
+        }
+    }
+}
